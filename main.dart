ff89032c48746a16d18a19a1386feeee51ef1f01
saferegion <128654331+saferegion@users.noreply.github.com>
2024-01-18 18:41:15 -0500

    changes

--- main.dart
+++ main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'app/modules/splashscreen/bindings/splashscreen_binding.dart';
+import 'app/routes/app_pages.dart';
 
 void main() async {
   runApp(const MyApp());
--- splashscreen_controller.dart
+++ splashscreen_controller.dart
@@ -1,21 +1,19 @@
-import 'package:flaury/app/routes/app_pages.dart';
 import 'package:get/get.dart';
 
 class SplashscreenController extends GetxController {
   //TODO: Implement SplashscreenController
 
   final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
 
-  
+  // @override
+  // Future<void> onReady() async {
+  //   await Future.delayed(const Duration(seconds: 3));
 
-  @override
-  void onClose() {
-    super.onClose();
-  }
+  //   Get.offAllNamed(Routes.ONBOARDING);
+
+  //   super.onReady();
+  // }
+
 
   void increment() => count.value++;
 }
